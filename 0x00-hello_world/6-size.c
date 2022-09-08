9a75155b84d4d255ad8de64b1fd05799e8f703b5
MeryemRouichi <rouichi.maryam@gmail.com>
2022-09-08 14:08:36 -0700

    printing the size of different variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main -Entry point
  *
- * C program that prints the size of various types on the computer it is compiled and run on
+ * C program that prints the size of variables
  *
  * Return: Always 0 (Success)
  */
