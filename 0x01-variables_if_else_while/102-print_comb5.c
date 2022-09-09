03704411df2c9630c42bd1ea51d80bab68ff81a9
MeryemRouichi <rouichi.maryam@gmail.com>
2022-09-09 15:47:44 -0700

    print all possible combinations of two two-digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all possible combinations of two two digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit1 = 0, digit2;
+
+	while (digit1 <= 99)
+	{
+		digit2 = digit1;
+		while (digit2 <= 99)
+		{
+			if (digit2 != digit1)
+			{
+				putchar((digit1 / 10) + '0');
+				putchar((digit1 % 10) + '0');
+				putchar(' ');
+				putchar((digit2 / 10) + '0');
+				putchar((digit2 % 10) + '0');
+				if (digit1 != 98 || digit2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
+	return (0);
+}
