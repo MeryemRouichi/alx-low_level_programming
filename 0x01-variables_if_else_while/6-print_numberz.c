b51820c99ae936faa73dafe37cba376bde5e1ff5
MeryemRouichi <rouichi.maryam@gmail.com>
2022-09-09 11:54:21 -0700

    print all digit numbers in base 10 using only putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: print all single digits in base 10 using only putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+
+	putchar('\n');
+	return (0);
+}
