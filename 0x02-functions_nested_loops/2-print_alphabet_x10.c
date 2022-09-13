00ae7bf98d0032ed7b32dbee73cf53e0a2e38849
MeryemRouichi <rouichi.maryam@gmail.com>
2022-09-13 06:24:57 -0700

    print alphabet letter 10x using _putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - uses _putchar to print alphabet letters 10x
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
