bf114f273f0c206625e06757f9863a706a7ca6fa
MeryemRouichi <rouichi.maryam@gmail.com>
2022-09-15 08:59:18 -0700

    fizz fuzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers from 0 to 100 except for the
+ * multiples of 3 should Fizz and multiples of 5 should
+ * print Fuzz
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && !(i % 5 == 0))
+			printf("Fizz");
+		else if (i % 5 == 0 && !(i % 3 == 0))
+			printf("Fuzz");
+		else if  (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
+}
