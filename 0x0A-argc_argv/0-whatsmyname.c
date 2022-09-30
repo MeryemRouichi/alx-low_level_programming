cfb840094b45ab3ee7497cfd72e4e86e92745928
MeryemRouichi <rouichi.maryam@gmail.com>
2022-09-30 09:30:25 -0700

    program that prints its own name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,13 +9,8 @@
  * Return: Always 0.
  */
 
-int main(int argc, char *argv[])
+int main(__attribute__((unused)) int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[0]);
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 }
