b9a9e99110296d59d1267d559ab5c1552fc9cf3e
MeryemRouichi <rouichi.maryam@gmail.com>
2022-09-30 09:09:04 -0700

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - assigning arguments to main function
+ *
+ * @argc: size of the argv
+ * @argv: array of size argc
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+main arguments: argc and argv
