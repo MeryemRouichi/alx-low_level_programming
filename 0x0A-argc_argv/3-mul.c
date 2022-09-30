5cfff8979542e3693acc17103ba165c1e9ad701b
MeryemRouichi <rouichi.maryam@gmail.com>
2022-09-30 10:24:03 -0700

    multiply wo argument numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,8 +12,6 @@
 
 int main(int argc, char *argv[])
 {
-	int i, mult;
-
 	if (argc < 2)
 	{
 		printf("Error\n");
@@ -21,11 +19,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		for (i = 1; i < 2; i++)
-		{
-			mult = atoi(argv[i]) * atoi(argv[i + 1]);
-		}
-		printf("%d\n", mult);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	return (0);
 }
